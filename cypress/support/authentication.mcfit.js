f39cc7ecc0976231436c7d6a6c0d74f9fd762dca
atiqamadsch <104157149+atiqamadsch@users.noreply.github.com>
2023-02-25 18:36:18 +0100

    mcfit login test

---
+++ cypress/support/authentication.mcfit.js
@@ -0,0 +1,5 @@
+export const standardUser = "atiq.amaj@gmail.com";
+export const problemUser = "problem_user";
+export const lockedOutUser = "locked_out_user";
+export const performanceGlitchUser = "performance_glitch_user";
+export const password = "Atiq@12345";
---
+++ cypress/support/commands.js
@@ -0,0 +1,49 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+const data = require("../fixtures/mcfit.json");
+import { loginPage } from "../pages/login";
+
+Cypress.Commands.add("login", (username, password) => {
+  cy.url().should("eq", data.loginPage.url);
+
+  cy.title().should("eq", data.loginPage.title);
+
+  loginPage
+    .getUsernameInput()
+    .should("be.visible")
+    .and("have.attr", "placeholder", data.loginPage.username)
+    .type(username);
+
+  loginPage
+    .getPasswordInput()
+    .should("be.visible")
+    .and("have.attr", "placeholder", data.loginPage.password)
+    .type(password);
+    
+  loginPage.getLoginButton().should("be.enabled").click();
+});
