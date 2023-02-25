217edbec96d44cf87be9c33731fba3ed109df465
atiqamadsch <104157149+atiqamadsch@users.noreply.github.com>
2023-02-26 00:45:23 +0100

    3 time working

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+const data = require("../fixtures/mcfit.json");
+
+
+
+describe("Login Page Test case suite - web elements validation", () => {
+  before("Navigate to the login page", () => {
+    // Given I navigate to the login page
+    cy.visit("/");
+  });
+  // First TC
+  it("Validate mcfit login page", () => {
+    //Then I should see the login page url
+    cy.url().should("eq", data.loginPage.url);
+
+    // And I should see page title
+    cy.title().should("eq", data.loginPage.title);
+  });
+
 })
